f7942fc1674ba2701a8243026ed0af78c2b3576c
kasratabrizi <kasra.tabrizi@netcentric.biz>
2023-07-04 21:12:30 +0200

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
 import * as React from "react";
 import "./styles/app.css";
+import { Context } from "./context";
 import SideNavBar from "./components/SideNavBar";
 import { Outlet } from "react-router-dom";
+import lessonImage from "./assets/two-people-talking.jpg";
+import lessonImage2 from "./assets/tapas-and-beer.jpg";
+
+const units = [
+  {
+    id: 1,
+    title: "Unit 1",
+    description: "Form basic sentences, greet people",
+    enabled: true,
+    finished: false,
+    lessons: [
+      {
+        id: 1,
+        title: "Lesson 1",
+        description: "Introduce yourself!",
+        avatar: lessonImage,
+        enabled: true,
+        finished: false,
+      },
+      {
+        id: 2,
+        title: "Lesson 2",
+        description: "Order food and drinks",
+        avatar: lessonImage2,
+        enabled: false,
+        finished: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Unit 2",
+    description: "Get around the city",
+    enabled: false,
+    finished: false,
+    lessons: [
+      {
+        id: 1,
+        title: "Lesson 1",
+        description: "Introduce yourself!",
+        avatar: lessonImage,
+        enabled: false,
+        finished: false,
+      },
+    ],
+  },
+];
 
 const App = () => {
   return (
-    <>
+    <Context.Provider value={units}>
       <SideNavBar />
       <div id="detail">
         <Outlet />
       </div>
-    </>
+    </Context.Provider>
   );
 };
 
--- src/routes/Learn.tsx
+++ src/routes/Learn.tsx
@@ -1,57 +1,13 @@
 import * as React from "react";
 import Unit from "../components/Unit/Unit";
-import lessonImage from "../assets/two-people-talking.jpg";
-import lessonImage2 from "../assets/tapas-and-beer.jpg";
-
-const units = [
-  {
-    id: 1,
-    title: "Unit 1",
-    description: "Form basic sentences, greet people",
-    enabled: true,
-    finished: false,
-    lessons: [
-      {
-        id: 1,
-        title: "Lesson 1",
-        description: "Introduce yourself!",
-        avatar: lessonImage,
-        enabled: true,
-        finished: false,
-      },
-      {
-        id: 2,
-        title: "Lesson 2",
-        description: "Order food and drinks",
-        avatar: lessonImage2,
-        enabled: false,
-        finished: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Unit 2",
-    description: "Get around the city",
-    enabled: false,
-    finished: false,
-    lessons: [
-      {
-        id: 1,
-        title: "Lesson 1",
-        description: "Introduce yourself!",
-        avatar: lessonImage,
-        enabled: false,
-        finished: false,
-      },
-    ],
-  },
-];
+import { useContext } from "react";
+import { Context } from "../context";
 
 const Learn = () => {
+  const units = useContext(Context);
   return (
     <div id="learn__container">
-      {units.map((unit) => {
+      {units.map((unit: any) => {
         return <Unit key={unit.id} unit={unit} />;
       })}
     </div>
