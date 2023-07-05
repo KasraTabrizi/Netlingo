47714dcde38f34e91529d33035a57f05d4245143
kasratabrizi <kasra.tabrizi@netcentric.biz>
2023-07-05 15:08:17 +0200

    add status to error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,13 +3,15 @@ import "../styles/errorpage.css";
 
 const ErrorPage = () => {
   const error: any = useRouteError();
-
+  console.log(error);
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {error.status} {error.statusText || error.message}
+        </i>
       </p>
     </div>
   );
